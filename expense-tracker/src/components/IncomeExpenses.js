68c800159b511b525a2aecc310579c221ffd37d0
Hari-Bombon <haripriya14022003@gmail.com>
2023-09-03 17:59:33 +0530

    Initialized the project

---
+++ expense-tracker/src/components/IncomeExpenses.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const IncomeExpenses = () => {
+  return (
+    <div class='inc-exp-container'>
+        <div>
+            <h4>Income</h4>
+            <p id="money-plus" class="money-plus">+$0.00</p>
+        </div>
+    </div>
+  )
+}
