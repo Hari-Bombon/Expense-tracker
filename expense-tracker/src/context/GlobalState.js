e0323bbc24f76c86f974a06273d2eae8ebfa6e89
Hari-Bombon <haripriya14022003@gmail.com>
2023-09-05 15:43:56 +0530

    expense-tracker

--- expense-tracker/src/context/GlobalState.js
+++ expense-tracker/src/context/GlobalState.js
@@ -1,27 +1,40 @@
 import React, { createContext, useReducer } from "react";
 import AppReducer from './AppReducer';
-//initial state
 
+// Initial state
 const initialState ={
-    transactions: [
-        {id: 1, text:'flower', amount:-20},
-        {id: 2, text:'salary', amount:300},
-        {id: 3, text:'Book', amount:-10},
-        {id: 4, text:'Camera', amount:150}
-    ]
+    transactions: []
 }
 
-/// create context
+// Create context
 export const GlobalContext = createContext(initialState);
 
-//Provider component
+// Provider component
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
 
-export const GlobalProvider =({children}) =>{
-    const [state, dispatch] =useReducer(AppReducer, initialState);
+    // Actions
+    function deleteTransaction(id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION', 
+            payload: id
+        });
+    }
 
-    return(<GlobalContext.Provider value={{
-        transactions:state.transactions
-    }}>
-        {children}
-    </GlobalContext.Provider>);
+    function addTransaction(transaction) {
+        dispatch({
+            type: 'ADD_TRANSACTION', 
+            payload: transaction
+        });
+    }
+
+    return (
+        <GlobalContext.Provider value={{
+            transactions: state.transactions,
+            deleteTransaction,
+            addTransaction
+        }}>
+            {children}
+        </GlobalContext.Provider>
+    );
 }
